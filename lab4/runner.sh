d4f49c8990f334e780ea44fad2bf8a48772a4815
ariapahlavan <apahlavan1@utexas.edu>
2017-04-27 18:06:34 -0500

    lost of bug fixes in lab4

---
+++ lab4/runner.sh
@@ -0,0 +1,2 @@
+make simulate
+./simulate ucode4 add.obj data.obj except_prot.obj except_unaligned.obj except_unknown.obj int.obj vector_table.obj < command > output
---
+++ lab4/vector_table.asm
@@ -0,0 +1,8 @@
+    .ORIG x0202
+
+    .FILL x1200     ; 0x0202
+    .FILL x1600     ; 0x0204
+    .FILL x1A00     ; 0x0206
+    .FILL x1C00     ; 0x0208
+    
+    .END
